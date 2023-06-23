893e6c89eaa430dd4dd442318925dd5e856d990e
Cyn <CynthiaThayer@pm.me>
2023-06-22 21:14:56 -0400

    Fix property name error

--- dotNETLemmy.API/Types/Forms/GetPostForm.cs
+++ dotNETLemmy.API/Types/Forms/GetPostForm.cs
@@ -4,7 +4,7 @@ public class GetPostForm : IForm
 {
     public string? Auth { get; set; }
     public int? CommentId { get; set; }
-    public int? PostId { get; set; }
+    public int? Id { get; set; }
 
     public string EndPoint => "/post";
     public HttpMethod Method => HttpMethod.Get;
